2bef59937bbb9e0b5d8d5ce04327aada7398bb14
Nithin L <nithinchitra2011@gmail.com>
2026-08-12 15:15:54 +0530

    Create 213-HouseRobberIi

---
+++ 213-HouseRobberIi/213-HouseRobberIi.java
@@ -0,0 +1,19 @@
+// Last updated: 8/12/2026, 3:15:57 PM
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(getMax(nums, 0, nums.length - 2), getMax(nums, 1, nums.length - 1));        
+    }
+
+    private int getMax(int[] nums, int start, int end) {
+        int prevRob = 0, maxRob = 0;
+
+        for (int i = start; i <= end; i++) {
+            int temp = Math.max(maxRob, prevRob + nums[i]);
+            prevRob = maxRob;
+            maxRob = temp;
+        }
+
+        return maxRob;
+    }    
+}
