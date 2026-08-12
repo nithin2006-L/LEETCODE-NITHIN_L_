d5c10ea6b24b1232a177fc358e58c387bf01214e
Nithin L <nithinchitra2011@gmail.com>
2026-08-12 15:13:06 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,15 @@
+// Last updated: 8/12/2026, 3:13:09 PM
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set= new HashSet<>();
+        for(int num:arr){
+            if(set.contains(2*num)||num%2==0&&set.contains(num/2)){
+                return true;
+            }
+            set.add(num);
+
+        }
+        return false;
+        
+    }
+}
