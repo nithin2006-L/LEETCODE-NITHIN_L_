de0a25165c38574c79b33dd81824a1f6bfea4d1e
Nithin L <nithinchitra2011@gmail.com>
2026-08-12 15:12:00 +0530

    Create 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum

---
+++ 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum/3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum.java
@@ -0,0 +1,20 @@
+// Last updated: 8/12/2026, 3:12:03 PM
+import java.util.HashSet;
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum = nums[0];
+        int i = 1;
+        while (i < nums.length && nums[i] == nums[i - 1] + 1) {
+            sum += nums[i];
+            i++;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        while (set.contains(sum)) {
+            sum++;
+        }
+        return sum;
+    }
+}
