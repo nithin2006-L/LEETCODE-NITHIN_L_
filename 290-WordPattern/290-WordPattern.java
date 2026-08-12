44e959f8eaa7d47a63146b9b2bb7232b3624809b
Nithin L <nithinchitra2011@gmail.com>
2026-08-12 15:15:13 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,33 @@
+// Last updated: 8/12/2026, 3:15:16 PM
+import java.util.HashMap;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        
+        HashMap<Character, String> charToWord = new HashMap<>();
+        HashMap<String, Character> wordToChar = new HashMap<>();
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+            
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) return false;
+            } else {
+                charToWord.put(c, word);
+            }
+            
+            if (wordToChar.containsKey(word)) {
+                if (wordToChar.get(word) != c) return false;
+            } else {
+                wordToChar.put(word, c);
+            }
+        }
+        
+        return true;
+    }
+}
